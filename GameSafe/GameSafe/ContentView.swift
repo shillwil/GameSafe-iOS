cc1e2264bc1e81a06aabd23583525537703c8529
Alex Shillingford <alex.shillingford@icloud.com>
2020-07-10 18:44:18 -0600

    Create Xcode Project

---
+++ GameSafe/GameSafe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GameSafe
+//
+//  Created by Alex Shillingford on 7/10/20.
+//  Copyright © 2020 Shillwil. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
